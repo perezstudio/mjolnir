8a07a6849a3a3ce9c990fc562587e89116b1fa7e
Kevin Perez <kp952547@gmail.com>
2026-02-18 20:39:16 -0600

    Initial Commit

---
+++ MjolnirTests/MjolnirTests.swift
@@ -0,0 +1,17 @@
+//
+//  MjolnirTests.swift
+//  MjolnirTests
+//
+//  Created by Keviruchis on 2/18/26.
+//
+
+import Testing
+@testable import Mjolnir
+
+struct MjolnirTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ MjolnirUITests/MjolnirUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  MjolnirUITestsLaunchTests.swift
+//  MjolnirUITests
+//
+//  Created by Keviruchis on 2/18/26.
+//
+
+import XCTest
+
+final class MjolnirUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
